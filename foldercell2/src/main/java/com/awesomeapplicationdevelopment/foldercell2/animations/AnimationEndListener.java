d450065f2492095e34338082cdee957c47096d2f
Krishna Purohit <krishna83599@gmail.com>
2019-04-21 07:06:37 -0700

    26 or 26+

---
+++ foldercell2/src/main/java/com/awesomeapplicationdevelopment/foldercell2/animations/AnimationEndListener.java
@@ -0,0 +1,18 @@
+package com.awesomeapplicationdevelopment.foldercell2.animations;
+
+
+import android.view.animation.Animation;
+
+public abstract class AnimationEndListener implements Animation.AnimationListener {
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // do nothing
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // do nothing
+    }
+
+}
